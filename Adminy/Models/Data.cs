5b437aee54eca22060e9f5b8f6f6c451d8cb2732
Mike Faro <ivyk96@live.com>
2016-01-20 14:42:34 +0100

    Able to enter a date with the data type string. Also added a reset.css file.

--- Adminy/Models/Data.cs
+++ Adminy/Models/Data.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Adminy
 {
     public class Data
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
         public string Date { get; set; }
+
         //public DateTime Start { get; set; }
         //public DateTime End { get; set; }
         //public float Salary { get; set; }
